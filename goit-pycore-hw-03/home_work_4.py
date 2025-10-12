21d32e7fc60b3243f8851da12851981774a57c23
froii <froii@ukr.net>
2025-10-12 18:29:17 +0300

    HW3 - work 4 - fixed format

--- goit-pycore-hw-03/home_work_4.py
+++ goit-pycore-hw-03/home_work_4.py
@@ -1,7 +1,6 @@
-
 from datetime import datetime, timedelta  
 
-DATE_FORMAT = '%Y-%m-%d'
+DATE_FORMAT = '%Y.%m.%d'
 
 def get_upcoming_birthdays(users: list[dict[str, str]]) -> list[dict[str, str]]:
     today = datetime.today().date()
@@ -26,23 +25,16 @@ def get_upcoming_birthdays(users: list[dict[str, str]]) -> list[dict[str, str]]:
     return birthdays
     
 
-
 users = [
-    {"name": "John Doe", "birthday": "1985-10-11"},         
-    {"name": "Jane Smith", "birthday": "1990-10-12"},       
-    {"name": "Alice Johnson", "birthday": "1988-10-13"},    
-    {"name": "Bob Williams", "birthday": "1992-09-14"},     
-    {"name": "Charlie Brown", "birthday": "1987-09-15"},    
-    {"name": "Diana Martinez", "birthday": "1995-09-16"},   
-    {"name": "Edward Davis", "birthday": "1989-09-17"},     
-    {"name": "Fiona Garcia", "birthday": "1991-09-18"},     
-    {"name": "George Wilson", "birthday": "1986-09-05"},
-    {"name": "Helen Clark", "birthday": "1993-10-14"},
-    {"name": "Ivan Petrov", "birthday": "1984-10-15"},
-    {"name": "Julia Roberts", "birthday": "1996-10-16"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"},
+    {"name": "Олег Коваль", "birthday": "1988.10.13"},        # понеділок
+    {"name": "Ірина Шевченко", "birthday": "1992.10.15"},     # середа
+    {"name": "Марія Іванова", "birthday": "1995.10.17"},      # п'ятниця
+    {"name": "Петро Сидоренко", "birthday": "1983.10.18"},    # субота - переноситься на понеділок )( +2 дні ) {'name': 'Петро Сидоренко', 'congratulation_date': '2025.10.20'}
+    {"name": "Анна Коваленко", "birthday": "1991.09.30"},
+    {"name": "Сергій Бондар", "birthday": "1987.11.18"}
 ]
 
-
 upcoming_birthdays = get_upcoming_birthdays(users)
-print("Список привітань на цьому тижні:", upcoming_birthdays)
-
+print("Список привітань на цьому тижні:", upcoming_birthdays) 
