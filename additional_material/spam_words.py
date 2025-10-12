8175201bd1a7d4c21f054b726a92cddd94f54477
froii <froii@ukr.net>
2025-10-12 13:57:19 +0300

    added world lenght to remove_spam_words

--- additional_material/spam_words.py
+++ additional_material/spam_words.py
@@ -11,16 +11,18 @@ Python is the best programming language!
 SPAM_WORDS_LIST = ["Python", "Guido van Rossum"]
 
 def remove_spam_words(message: str) -> str:
+    def set_len_stars(match: re.Match) -> str:
+        return '*' * len(match.group(0))
     for spam_word in SPAM_WORDS_LIST:
-        message = re.sub(rf"\b{spam_word}\b", "*", message)
+        message = re.sub(rf"\b{spam_word}\b", set_len_stars, message)
     return message
 
 
 
 
-assert remove_spam_words("Guido van Rossum") == '*'
-assert remove_spam_words("Python") == '*' # '******'
-assert remove_spam_words("Guido van Rossum Python") == '* *'
+assert remove_spam_words("Guido van Rossum") == '****************' # довжина слова len('Guido van Rossum') = 16
+assert remove_spam_words("Python") == '******' # '******' # довжина слова len('Python') = 6
+assert remove_spam_words("Guido van Rossum Python") == '**************** ******' # '**************** ******' # довжина слова len('Guido van Rossum') = 16, len('Python') = 6
 
 print(remove_spam_words("Guido van Rossum began working on Python "
 "in the late 1980s as a successor to the ABC programming language "
